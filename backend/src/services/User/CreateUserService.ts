fda8c8870d76004012655ea41637266ba0cb8f61
Murilo Melo <murilofsantosm@gmail.com>
2023-10-16 14:15:12 -0300

    feature
    feature

--- backend/src/services/User/CreateUserService.ts
+++ backend/src/services/User/CreateUserService.ts
@@ -1,15 +1,48 @@
-interface UserRequest{
-    name: string;
-    email: string;
-    password: string;
+import prismaClient from "../../prisma";
+import { hash } from "bcryptjs";
+
+interface UserRequest {
+  name: string;
+  email: string;
+  password: string;
 }
 
-
-class CreateUserService{
-    async execute({name, email, password}: UserRequest){
-        console.log(name);
-        return { name: name, email: email, password: password}
+class CreateUserService {
+  async execute({ name, email, password }: UserRequest) {
+    // verificar se ele enviou um e-mail
+    if (!email) {
+      throw new Error("Email incorrect");
     }
+
+    // Verificar se o email já está cadastrado na plataforma
+
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: passwordHash,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    return user;
+  }
 }
 
-export { CreateUserService }
+export { CreateUserService };
