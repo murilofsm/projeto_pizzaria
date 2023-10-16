9372c7707740ba90ba9a28d9903059a1b13f8cef
Murilo Melo <96501309+murilofsm@users.noreply.github.com>
2023-10-15 23:18:48 -0300

    feature
    adicionado o prisma para a criação do banco de dados

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,7 +3,9 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/test", (req: Request, res: Response) => {
-  return res.json({ ok: true });
+
+    return res.json({ nome: 'Murillus Pizza' });
+//   throw new Error("Erro ao fazer essas requisição");
 });
 
 export { router };
